9510c012173f12c6138de6af3852d7f2f0115430
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-07-16 21:21:36 -0400

    clean up javascript

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 var clock = document.getElementById("clock")
 var bg = document.getElementById("bg-color")
 
+function newNum(num) {
+  return num <= 10 ? "0"+num : num
+}
 
 function itsTime() {
   var time = new Date()
   var hours = time.getHours()
   var minutes = time.getMinutes()
   var seconds = time.getSeconds()
-  
-  if(hours <= 9) {
-    hours = "0" + hours
-  }
-  
-  if(minutes <= 9) {
-    minutes = "0" + minutes
-  }
-  
-  if(seconds <= 9) {
-    seconds = "0" + seconds
-  }
-  
-  var hex = `#${hours}${minutes}${seconds}`
+
+  var hex = `#${newNum(hours)}${newNum(minutes)}${newNum(seconds)}`
   
   bg.style.backgroundColor = hex
   
-  clock.innerHTML = `${hours}:${minutes}:${seconds}`
+  clock.innerHTML = `${newNum(hours)}:${newNum(minutes)}:${newNum(seconds)}`
 }
 
 setInterval(itsTime, 1000)
